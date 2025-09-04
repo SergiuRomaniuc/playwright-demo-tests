b0ad9389585153f409484d7c6d0e2e612842d43f
SergiuRomaniuc <sergiu.romaniuc01@gmail.com>
2025-09-04 12:38:18 +0300

    add first test

--- tests/asigTest.spec.ts
+++ tests/asigTest.spec.ts
@@ -1,7 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Locator } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://zota.team/');
+test.skip('has title', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/?utm_source=chatgpt.com#/');
 
-  await expect(page).toHaveTitle(/zota.team/);
+  await expect(page).toHaveTitle(/React • TodoMVC/);
+});
+
+test('input is clikable', async ({ page }) => {
+    await page.goto('https://demo.playwright.dev/todomvc/?utm_source=chatgpt.com#/');
+
+    let locatorInputField:Locator = page.locator('.new-todo');
+
+    let isVisible:boolean = await locatorInputField.isVisible();
+    // let isVisible:boolean = false;
+    let isEnabled:boolean = await locatorInputField.isEnabled();
+    // let isEnabled:boolean = true;
+
+    if (isVisible && isEnabled) {
+        console.log("✅ The element is visible and enabled.");
+        expect(isVisible && isEnabled).toBeTruthy();
+    } else {
+        console.log("❌ The element is not visible or not enabled.");
+        expect(isVisible && isEnabled).toBeTruthy();
+    }
 });
